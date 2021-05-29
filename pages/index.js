2389221dae3bd3f14d92a5d0aa1cb87e7d92eda1
jeroenvanhattem <jeroenvanhattem@outlook.com>
2021-05-29 14:37:35 +0200

    Added login to /index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 const HomePage = () => {
+  const router = useRouter()
+  const loginHandler = () => {
+    router.push('/login')
+  }
   return (
-    <>
+    <div className={styles.container}>
       <h1>HomePage</h1>
-    </>
+      <button onClick={loginHandler}>Login</button>
+    </div>
   )
 }
 
