5f6febb2e05e13f31d0c60850ba095e51b56ef58
jeroenvanhattem <jeroenvanhattem@outlook.com>
2021-06-05 16:34:17 +0200

    Fixed Vercel error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  webpack: (config, options) => {
-    config.module.rules.push(
-      {
-        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-        loader: 'url-loader?limit=100000' 
-      }
-    )
-    return config
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
   env: {
     API_KEY: process.env.API_KEY,
